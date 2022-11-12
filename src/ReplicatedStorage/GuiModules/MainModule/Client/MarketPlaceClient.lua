0e4ee0266fdc783d2bf95583a1ed8c30ef3ef99c
jjmaxman <116936067+jjmaxman@users.noreply.github.com>
2022-11-11 23:48:32 -0800

    Update MarketPlaceClient.lua

--- src/ReplicatedStorage/GuiModules/MainModule/Client/MarketPlaceClient.lua
+++ src/ReplicatedStorage/GuiModules/MainModule/Client/MarketPlaceClient.lua
@@ -2,18 +2,18 @@ local globals = require(game:GetService("ReplicatedStorage").Globals)
 local plr = globals.Players.LocalPlayer
 local plrGui = plr.PlayerGui
 local marketPlaceGui = plrGui:WaitForChild("Market")
-local mainFrame = marketPlaceGui.MainFrame
-local scrollingFrameList: ScrollingFrame = mainFrame.ScrollingFrameList
+local mainFrame = marketPlaceGui.Market
+local scrollingFrameList: ScrollingFrame = mainFrame.ScrollingFrameListMarket
 local scrollingFrameMarket = mainFrame.ScrollingFrameMarket
 
 local module = {}
 function module.LoadModule()
     scrollingFrameList.MouseEnter:Connect(function()
-        scrollingFrameList:TweenPosition(UDim2.new(0,0,0.181,0), Enum.EasingDirection.Out, Enum.EasingStyle.Quint, .5, true)
+        scrollingFrameList:TweenPosition(UDim2.new(0,0,0.216,0), Enum.EasingDirection.Out, Enum.EasingStyle.Quint, .5, true)
     end)
 
     scrollingFrameList.MouseLeave:Connect(function()
-        scrollingFrameList:TweenPosition(UDim2.new(-.1,0,.181,0), Enum.EasingDirection.Out, Enum.EasingStyle.Quint, .5, true)
+        scrollingFrameList:TweenPosition(UDim2.new(-.1,0,0.216,0), Enum.EasingDirection.Out, Enum.EasingStyle.Quint, .5, true)
     end)
 end
 return module
